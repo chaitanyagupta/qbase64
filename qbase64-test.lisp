cf135f857cccfdf2fa7fb2b87363b07bbb288bdf
Chaitanya Gupta <mail@chaitanyagupta.com>
2017-11-23 18:19:32 +0530

    Update tests to check linebreak input and output

--- qbase64-test.lisp
+++ qbase64-test.lisp
@@ -8,11 +8,11 @@
 
 ;;; utils
 
-(defun external-encode (bytes)
+(defun external-encode (bytes &key (linebreak 0))
   (temporary-file:with-open-temporary-file (tmp :direction :output :element-type '(unsigned-byte 8))
     (write-sequence bytes tmp)
     (force-output tmp)
-    (uiop:run-program `("base64" "-i" ,(namestring tmp)) :output '(:string :stripped t))))
+    (uiop:run-program `("base64" "-b" ,(format nil "~A" linebreak) "-i" ,(namestring tmp)) :output (if (zerop linebreak) '(:string :stripped t) :string))))
 
 (let ((gen (gen-integer :min 0 :max 255)))
   (defun random-byte ()
@@ -22,6 +22,8 @@
   (let ((bytes (make-byte-vector size)))
     (dotimes (i size bytes)
       (setf (aref bytes i) (random-byte)))))
+
+(defparameter *newline-bag* (format nil "~A~A~A" #\Newline #\Linefeed #\Return))
 
 ;;; encoder tests
 
@@ -33,6 +35,16 @@
            (encoded (qbase64:encode-bytes bytes))
            (external-encoded (external-encode bytes)))
       (is (string= external-encoded encoded)
+          "Failed for size ~A: Expected ~S for ~S, but got ~S"
+          size external-encoded bytes encoded))))
+
+(test (encode-bytes-with-linebreaks :suite encoder)
+  (dolist (size (list 0 50 75 100 125 150 200))
+    (let* ((bytes (random-bytes size))
+           (encoded (qbase64:encode-bytes bytes :linebreak 50))
+           (external-encoded (external-encode bytes :linebreak 50)))
+      (is (string= (string-trim *newline-bag* external-encoded)
+                   (string-trim *newline-bag* encoded))
           "Failed for size ~A: Expected ~S for ~S, but got ~S"
           size external-encoded bytes encoded))))
 
@@ -57,6 +69,15 @@
   (dolist (size (list 0 1 2 3 4 5 6 7 8 9 10 100))
     (let* ((bytes (random-bytes size))
            (string (external-encode bytes))
+           (decoded (qbase64:decode-string string)))
+      (is (equalp bytes decoded)
+          "Failed for size ~A: Expected ~S for ~S, but got ~S"
+          size bytes string decoded))))
+
+(test (decode-string-with-linebreaks :suite decoder)
+  (dolist (size (list 0 50 75 100 125 150 200))
+    (let* ((bytes (random-bytes size))
+           (string (external-encode bytes :linebreak 50))
            (decoded (qbase64:decode-string string)))
       (is (equalp bytes decoded)
           "Failed for size ~A: Expected ~S for ~S, but got ~S"
